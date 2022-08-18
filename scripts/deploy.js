cfc0069596110f116fb1e27ff6e5103557f000b8
Arik Chakma <arikchangma@gmail.com>
2022-08-18 15:45:21 +0600

    chore: contract deployed

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy.
+  const BuyMeAChai = await hre.ethers.getContractFactory("BuyMeAChai");
+  const buyMeAChai = await BuyMeAChai.deploy();
+
+  // Deploy the contract.
+  await buyMeAChai.deployed();
+  console.log("BuyMeAChai deployed to:", buyMeAChai.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
